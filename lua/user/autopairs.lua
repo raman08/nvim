030457fb1ec05e49ddc671dbcedaa861803e8ddd
Raman Preet Singh <ramanpre0810@gmail.com>
2022-09-05 02:09:16 +0530

    autopairs handled

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -8,7 +8,7 @@ npairs.setup {
         javascript = {"string", "template_string"},
         -- java = false,
     },
-    disable_filetype = {"TelescopePrompt"},
+    disable_filetype = {"TelescopePrompt", "spectre_pannel"},
     fast_wrap = {
         map = "<M-e>",
         chars = {"{", "[", "(", "\"", "'"},
@@ -22,9 +22,8 @@ npairs.setup {
     },
 }
 
--- local cmp_autopairs = require "nvim-autopairs.completion.cmp"
--- local cmp_status_ok, cmp = pcall(require, "cmp")
--- if not cmp_status_ok then
---   return
--- end
--- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then return end
+cmp.event:on("confirm_done",
+             cmp_autopairs.on_confirm_done {map_char = {tex = ""}})
