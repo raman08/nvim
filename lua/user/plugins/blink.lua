b7d88f518ee1b0cb94b47b0f7c994ce8680a2c33
Raman Preet Singh <ramanpre0810@gmail.com>
2024-12-29 21:38:06 +0530

    fix(blink): fix tab behaviour

--- lua/user/plugins/blink.lua
+++ lua/user/plugins/blink.lua
@@ -20,6 +20,8 @@ function M.config()
 	local opts = {
 		keymap = {
 			preset = "enter",
+			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
 			["<C-j>"] = { "select_next", "fallback" },
 			["<C-k>"] = { "select_prev", "fallback" },
 		},
